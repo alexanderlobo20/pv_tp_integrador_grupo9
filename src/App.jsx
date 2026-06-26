97af0c4587f362ca3a06bfe32b742473b500b59c
fernando-eb2406 <feb-332@hotmail.com>
2026-06-26 16:50:08 -0300

    feat: crear vista de lista de clientes e implementar peticion GET

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./views/Login";
-import Dashboard from "./views/Dashboard";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import Header from "./components/layout/Header";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './views/Login';
+import Dashboard from './views/Dashboard';
+import ProtectedRoute from './routes/ProtectedRoute';
+import Header from './components/layout/Header';
+import ListaClientes from './views/ListaClientes';
 
 export default function App() {
   return (
@@ -10,10 +11,13 @@ export default function App() {
       <Header />
 
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
 
         <Route
-          path="/"
+          path='/'
           element={
             <ProtectedRoute>
               <Dashboard />
@@ -21,7 +25,15 @@ export default function App() {
           }
         />
 
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route
+          path='/clientes'
+          element={<ListaClientes />}
+        />
+
+        <Route
+          path='*'
+          element={<Navigate to='/' />}
+        />
       </Routes>
     </>
   );
--- src/views/ListaClientes.jsx
+++ src/views/ListaClientes.jsx
@@ -1,24 +1,82 @@
-import { useState } from "react";
-import Button from "@mui/material/Button";
-import ModuloC from "../components/common/ModuloC";
-import Dialog from "@mui/material/Dialog";
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import ModuloC from '../components/common/ModuloC';
+import Dialog from '@mui/material/Dialog';
+import { useEffect } from 'react';
 
 function ListaClientes() {
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
 
-    const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const handleOpen = () => setMostrarFormulario(true);
+  const handleClose = () => setMostrarFormulario(false);
 
-    const handleOpen = () => setMostrarFormulario(true);
-    const handleClose = () => setMostrarFormulario(false);
+  const [clientes, setClientes] = useState([]);
+  const [estado, setEstado] = useState('');
 
-    return (
-        <>
-            <Button variant="contained" onClick={handleOpen}>Ingresar nuevo Cliente</Button>
+  useEffect(() => {
+    const obtenerClientes = async () => {
+      try {
+        setEstado('carga');
+        const response = await fetch('https://fakestoreapi.com/users');
+        if (!response.ok) {
+          throw new Error(
+            `Error ${response.status}: no se pudo obtener clientes`,
+          );
+        }
+        const data = await response.json();
+        setClientes(data);
+        setEstado('exito');
+      } catch (error) {
+        setEstado('error');
+      }
+    };
+    obtenerClientes();
+  }, []);
 
-            <Dialog open={mostrarFormulario} onClose={handleClose} maxWidth="md" fullWidth PaperProps={{sx: { borderRadius: 4 }}}>
-                <ModuloC onCerrar={handleClose} onSuccess={handleClose} />
-            </Dialog>
-        </>
-    );
+  return (
+    <>
+      <Button
+        variant='contained'
+        onClick={handleOpen}
+      >
+        Ingresar nuevo Cliente
+      </Button>
+
+      <Dialog
+        open={mostrarFormulario}
+        onClose={handleClose}
+        maxWidth='md'
+        fullWidth
+        PaperProps={{ sx: { borderRadius: 4 } }}
+      >
+        <ModuloC
+          onCerrar={handleClose}
+          onSuccess={handleClose}
+        />
+      </Dialog>
+
+      <section>
+        <h1>Lista de Clientes</h1>
+
+        {estado === 'carga' && <p>Cargando...</p>}
+        {estado === 'error' && <p>Error al cargar clientes</p>}
+        {estado === 'exito' && (
+          <ul>
+            {clientes.map((cliente) => (
+              <li key={cliente.id}>
+                <h2>
+                  {cliente.name.firstname} {cliente.name.lastname}
+                </h2>
+                <p>{cliente.address.city}</p>
+                <button>Editar</button>
+                <button>Eliminar</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </>
+  );
 }
 
 export default ListaClientes;
