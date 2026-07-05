c3f1ae27cc6cfb8596339739663d9b551c594470
florencia villar <florenciavillar14@gmail.com>
2026-07-05 19:44:45 -0300

    feat: Incorporación de ClientesContext.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AdminProvider } from "./context/AdminContext.jsx";
+import { ClienteProvider } from "./context/ClientesContext.jsx";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AdminProvider>
+      <ClienteProvider>
       <App />
+      </ClienteProvider>
     </AdminProvider>
   </BrowserRouter>
 );
